ff6042b331321c6a6e9f0172ee14884251207283
mucahityaman <mucahit.yaman@hotmail.com>
2026-04-13 21:12:25 +0300

    feat: add database seeding step to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "▶ Running database migrations..."
 NODE_ENV=production ./node_modules/.bin/knex migrate:latest --knexfile dist/knexfile.js
 
+echo "▶ Running database seeds..."
+NODE_ENV=production ./node_modules/.bin/knex seed:run --knexfile dist/knexfile.js
+
 echo "▶ Starting API server..."
 exec node dist/src/index.js
